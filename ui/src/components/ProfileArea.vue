9625572abf748a3f89f24787d10a35ccd5a9c0f9
Ajdj100 <ajcampbell@conestogac.on.ca>
2025-04-10 14:22:02 -0400

    feat: added dedicated profile box on main page

---
+++ ui/src/components/ProfileArea.vue
@@ -0,0 +1,18 @@
+<script setup>
+import router from '@/router';
+
+
+function logout() {
+    router.push("/login");
+}
+
+</script>
+
+
+<template>
+    <div class="flex flex-row justify-between bg-white border-1 border-gray-300 rounded-md p-2 px-4">
+        <div>Name</div>
+
+        <button @click="logout" class="cursor-pointer">Logout</button>
+    </div>
+</template>
