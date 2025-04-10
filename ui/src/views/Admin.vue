bf399a4f5e96bb6779ed2369ec00a68fe312754d
Ajdj100 <ajcampbell@conestogac.on.ca>
2025-04-10 14:23:10 -0400

    feat: added button to return to home from admin page

--- ui/src/views/Admin.vue
+++ ui/src/views/Admin.vue
@@ -1,10 +1,12 @@
 <script setup>
+import router from '@/router';
 import { RouterLink, RouterView } from 'vue-router';
 
 
 </script>
 
 <template>
+    <button class="absolute left-40 top-10 cursor-pointer" @click="router.push('/')">Back</button>
     <div class="flex flex-row max-w-[1000px] mx-auto h-screen p-20">
         <!-- sidebar -->
         <div class="bg-red-400 w-3xs h-full p-5">
