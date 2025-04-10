44b8c9a041b7bf2ec8e682c7d7065efecb016c68
Ajdj100 <ajcampbell@conestogac.on.ca>
2025-04-10 14:23:53 -0400

    feat: visual update to home page

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ProfileArea from '@/components/ProfileArea.vue';
 import { RouterLink } from 'vue-router';
 
 </script>
@@ -7,27 +8,30 @@ import { RouterLink } from 'vue-router';
   <!-- main separator -->
   <div class="flex flex-row h-screen">
     <!-- sidebar -->
-    <div class="w-xs h-full bg-amber-100 flex flex-col">
+    <div class="w-xs h-full bg-neutral-100 flex flex-col p-7 border-r-1 border-r-gray-200">
       <!-- logo -->
-      <div class="">
+      <div class="mx-auto p-4">
         Logo Placeholder
       </div>
       <!-- admin controls -->
       <div>
         <RouterLink to="/admin">Admin button</RouterLink>
       </div>
+      <div>
+        <RouterLink to="">Queries</RouterLink>
+      </div>
       <!-- navigation stuff -->
       <div class="flex flex-col">
-        Navigation stuff
+        Spaces
         <RouterLink to="/">Link</RouterLink>
+      </div>
+      <!-- header bar -->
+      <div class="mt-auto">
+        <ProfileArea></ProfileArea>
       </div>
     </div>
     <!-- right side -->
-    <div class="flex flex-col">
-      <!-- header bar -->
-      <div>
-        <button>Logout</button>
-      </div>
+    <div class="flex flex-col p-10">
       <!-- main content area -->
       <div>
         THERE SHOULD BE CONTENT HERE BUT WE HAVE NO CONTENT TO PUT HERE YET
