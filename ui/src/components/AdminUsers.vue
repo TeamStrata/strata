1c106acc166cc34af808119f67aa44106e3cf166
Ajdj100 <ajcampbell@conestogac.on.ca>
2025-04-13 13:44:17 -0400

    feat: another style pass over the users page

--- ui/src/components/AdminUsers.vue
+++ ui/src/components/AdminUsers.vue
@@ -47,48 +47,63 @@ function addUser() {
     <div class="static" v-if="addModal">
         <div class="bg-black/20 w-screen h-screen absolute left-0 top-0 backdrop-blur-xs" @click="addModal = false">
         </div>
-        <div class="bg-white p-5 left-1/2 transform -translate-x-1/2 top-1/3 -translate-y-1/2 absolute rounded-lg">
-            <h1>Add User</h1>
+        <div
+            class="bg-white p-8 w-96 m-auto rounded-lg shadow-md absolute left-1/2 transform -translate-x-1/2 top-1/3 -translate-y-1/2">
+            <h1 class="text-2xl font-semibold text-gray-800 mb-6">Add User</h1>
             <div>
-                <label for="newUser">Username:</label>
-                <input id="newUser" v-model="newUsername">
+                <label for="newUser" class="text-gray-600">Username</label>
+                <input id="newUser" v-model="newUsername"
+                    class="mt-1 border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    placeholder="Enter username">
             </div>
-            <div>
-                <label for="newPass">Password:</label>
-                <input id="newPass" type="password" v-model="newPassword">
+            <div class="mt-4">
+                <label for="newPass" class="text-gray-600">Password</label>
+                <input id="newPass" type="password" v-model="newPassword"
+                    class="mt-1 border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    placeholder="Enter password">
             </div>
-            <div class="flex flex-row justify-between mt-4">
-                <button class="bg-neutral-200 px-2 rounded-sm cursor-pointer" @click="addModal = false">Cancel</button>
-                <button class="bg-blue-100 rounded-sm w-fit px-2 cursor-pointer" @click="addUser">Add User</button>
+            <div class="flex flex-row justify-between mt-6">
+                <button
+                    class="bg-neutral-200 px-4 py-2 rounded-md cursor-pointer hover:bg-neutral-300 focus:outline-none focus:ring-2 focus:ring-gray-300"
+                    @click="addModal = false">Cancel</button>
+                <button
+                    class="bg-blue-500 text-white py-2 px-4 rounded-md cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-200"
+                    @click="addUser">Add User</button>
             </div>
-
         </div>
     </div>
 
     <Toast ref="toastRef" />
 
-    <p>Maybe some flavor text</p>
+    <p class="text-gray-700 mb-4">Maybe some flavor text</p>
 
     <!-- list header -->
-    <div class="flex flex-row justify-between items-center mb-2">
-        <h1 class="font-bold">Manage Users</h1>
+    <div class="flex flex-row justify-between items-center mb-6">
+        <h1 class="text-2xl font-semibold text-gray-800">Manage Users</h1>
 
 
-        <div class="flex flex-row pb-2 items-center *:mx-3">
-            <p>{{ users.length }} users</p>
-            <input placeholder="search" class="p-2 rounded-lg">
-            <button @click="addModal = true" class="bg-amber-200 p-2 px-4 rounded-lg cursor-pointer">Add User</button>
+        <div class="flex flex-row pb-2 items-center space-x-3">
+            <p class="text-gray-600">{{ users.length }} users</p>
+            <input placeholder="Search users"
+                class="p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-64"
+                type="text">
+            <button @click="addModal = true"
+                class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-200 cursor-pointer">
+                Add User
+            </button>
         </div>
 
     </div>
 
     <!-- list body -->
     <ul>
-        <li class="test border-t-2 border-neutral-200 py-3 flex flex-row justify-between px-5" v-for="u in users">
-            {{ u.name }}
+        <li class="border-t-2 border-neutral-200 py-4 flex flex-row justify-between items-center px-5 hover:bg-gray-50 transition-colors"
+            v-for="u in users">
+            <span class="text-gray-800 font-medium">{{ u.name }}</span>
             <div class="flex items-center">
-                <p class="mr-25">User Type</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 20 20">
+                <p class="text-gray-600 mr-4">User Type</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 20 20"
+                    class="text-gray-600">
                     <path fill="currentColor"
                         d="M5 6a4 4 0 1 1 8 0a4 4 0 0 1-8 0m-3 7c0-1.113.903-2 2.009-2h6.248A5.48 5.48 0 0 0 9 14.5c0 1.303.453 2.5 1.21 3.443Q9.617 18 9 18c-1.855 0-3.583-.386-4.865-1.203C2.833 15.967 2 14.69 2 13m17 1.5a4.5 4.5 0 1 1-9 0a4.5 4.5 0 0 1 9 0m-2.646-1.146a.5.5 0 0 0-.708-.708L14.5 13.793l-1.146-1.147a.5.5 0 0 0-.708.708l1.147 1.146l-1.147 1.146a.5.5 0 0 0 .708.708l1.146-1.147l1.146 1.147a.5.5 0 0 0 .708-.708L15.207 14.5z" />
                 </svg>
