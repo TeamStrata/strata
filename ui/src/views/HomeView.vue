010abf27d870bf6a4ae2c42c96036cf51e3c08ec
ajdj100 <ajairplane@gmail.com>
2025-06-24 11:59:47 -0400

    feat: made options button hide on non-configurable pages

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -90,7 +90,10 @@ const pageInfo = computed(() => {return route.meta})
           <p class="leading-7">{{ pageInfo.description }}</p>
         </div>
         <!-- TODO: make this a real dropdown menu and button interaction and stuff -->
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12h16M4 18h16M4 6h16"/></svg>
+        <!-- Hidden options button that only shows on pages with the "configurable" property (for dashboard options) -->
+        <div :class="pageInfo.configurable ? 'block' : 'hidden'">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12h16M4 18h16M4 6h16"/></svg>
+        </div>
       </div>
       <Separator></Separator>
       <!-- main content area -->
