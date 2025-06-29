edef7e1e6e85f48a44de33d704eebb0fa1058eb8
ajdj100 <ajairplane@gmail.com>
2025-06-29 17:38:38 -0400

    feat: reimplemented the logged in state check and redirects

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import { useUserStore } from '@/stores/user'
+
+
+
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -65,5 +71,14 @@ const router = createRouter({
     }
   ],
 })
+
+router.beforeEach(async (to, from) => {
+  const store = useUserStore()
+  if (!store.isLoggedIn && to.name !== 'login') {
+    // redirect the user to the login page
+    return { name: 'login' }
+  }
+})
+
 
 export default router
