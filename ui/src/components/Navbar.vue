8fcf504e36a8ff6182cd55cb4bc997286c469974
ajdj100 <ajairplane@gmail.com>
2025-06-30 01:49:19 -0400

    chore: quick naming fix

--- ui/src/components/Navbar.vue
+++ ui/src/components/Navbar.vue
@@ -114,12 +114,12 @@ const user = useUserStore()
                 </SidebarGroupContent>
             </SidebarGroup>
             <SidebarGroup>
-                <SidebarGroupLabel>Projects</SidebarGroupLabel>
-                <SidebarGroupAction title="Add Project">
+                <SidebarGroupLabel>Dashboards</SidebarGroupLabel>
+                <SidebarGroupAction title="Add Dashboards">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                             stroke-width="2" d="M5 12h14m-7-7v14" />
-                    </svg> <span class="sr-only">Add Project</span>
+                    </svg> <span class="sr-only">Add Dashboards</span>
                 </SidebarGroupAction>
                 <SidebarContent>
                     TODO: dashboards go here
