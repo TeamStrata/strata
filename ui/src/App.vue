06ce436eb1873447a1f8804be7ba9f84c9e98181
Aj <ajairplane@gmail.com>
2025-08-01 08:13:19 -0400

    feat: implemented on load auth check, no more lurking without session (#145)

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { apiFetch } from './api/request';
+import router from './router';
+
+apiFetch("/auth", "POST").then(res => {
+  if (res.status != 200) {
+    router.push('/login');
+  }
+}).catch(error => {
+  console.error("Error during authentication:", error);
+})
 </script>
 
 <template>
