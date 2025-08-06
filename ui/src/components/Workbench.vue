3fc926f838ba4ed084cfc07a5d56f6593f0d70d2
ajdj100 <ajairplane@gmail.com>
2025-08-06 15:41:13 -0400

    fix: fixed chatbox size issue on message send

--- ui/src/components/Workbench.vue
+++ ui/src/components/Workbench.vue
@@ -7,19 +7,20 @@ import { apiFetch } from "@/api/request";
 
 
 
-    const userInput = ref('')
-    const response = ref('')
-    const error = ref('')
-    const loading = ref(false)
-    const messages = ref([]);
-    const schema = ref('');
+const userInput = ref('')
+const response = ref('')
+const error = ref('')
+const loading = ref(false)
+const messages = ref([]);
+const schema = ref('');
 
-    // Fetch the schema
-    apiFetch("/cdb-schema").then(async res => {
-      if (res.ok) {
-        schema.value = await res.text();
+// Fetch the schema
+apiFetch("/cdb-schema").then(async res => {
+  if (res.ok) {
+    schema.value = await res.text();
 
-        messages.value.push({"role": "system", "content": `
+    messages.value.push({
+      "role": "system", "content": `
 Your task is to convert a question into a SQL query, given a Postgres database schema.
 Adhere to these rules:
 - **Deliberately go through the question and database schema word by word** to appropriately answer the question
@@ -36,45 +37,46 @@ This query will run on a database whose schema is represented by this string:
 ${schema.value}
 \`\`\`sql
 `})
-      } else {
-        throw new Error(`Error ${res.status}: ${res.statusText}`);
-    }
-    });
+  } else {
+    throw new Error(`Error ${res.status}: ${res.statusText}`);
+  }
+});
 
-    const API_URL = '/stratabot'
-    const sendQuery = async () => {
-      if (!userInput.value.trim()) return
+const API_URL = '/stratabot'
+const sendQuery = async () => {
+  if (!userInput.value.trim()) return
 
-      loading.value = true
-      response.value = ''
-      error.value = ''
+  loading.value = true
+  response.value = ''
+  error.value = ''
 
-      // if the messages list is empty or the last message was not sent by the user...
-      if (messages.value.length == 0 || messages.value[messages.value.length - 1].role != "user") {
+  // if the messages list is empty or the last message was not sent by the user...
+  if (messages.value.length == 0 || messages.value[messages.value.length - 1].role != "user") {
     messages.value.push({ "role": "user", "content": userInput.value })
-      }
-      
-      userInput.value = '';
+  }
 
-      // Send the message history to ollama
+  userInput.value = '';
+  scaleChatBox();
+
+  // Send the message history to ollama
   apiFetch(API_URL, 'POST', JSON.stringify({ "messages": messages.value })).then(async (res) => {
-        // Mark that we are finished loading.
-        loading.value = false
+    // Mark that we are finished loading.
+    loading.value = false
 
-        if (!res.ok) {
-          // In the case of an error, push the error as a message
-          let err = `Error ${res.status}: ${res.statusText}`;
+    if (!res.ok) {
+      // In the case of an error, push the error as a message
+      let err = `Error ${res.status}: ${res.statusText}`;
       messages.value.push({ "role": "error", "content": err });
-          throw new Error(err);
+      throw new Error(err);
 
-        } else {
-          // In the case of a success, push the response
-          const data = await res.json()
+    } else {
+      // In the case of a success, push the response
+      const data = await res.json()
 
-          // Append the message to the message list
-          messages.value.push(data)
-        }
-      });
+      // Append the message to the message list
+      messages.value.push(data)
+    }
+  });
 
 
 }
@@ -99,7 +101,7 @@ function scaleChatBox() {
     </div>
 
     <Separator></Separator>
-    
+
     <div class="p-2 flex-1 overflow-y-scroll overflow-x-hidden">
       <div v-for="(message, index) in messages" :key="index" class="text-sm flex flex-col items-start">
         <p v-if="message.role != 'system'" class="block p-1 font-black"
@@ -108,7 +110,7 @@ function scaleChatBox() {
         </p>
 
         <div v-if="message.role != 'system'" class="w-fit rounded-sm p-2 max-w-4/5"
-        :class="message.role == 'user' ? 'bg-blue-300 self-end' : message.role == 'assistant' ? 'bg-purple-300 self-start' : 'bg-red-400'">
+          :class="message.role == 'user' ? 'bg-blue-300 self-end' : message.role == 'assistant' ? 'bg-purple-300 self-start' : 'bg-red-400'">
           <p class="block">{{ message.content }}</p>
         </div>
       </div>
@@ -124,7 +126,7 @@ function scaleChatBox() {
       <textarea placeholder="Write a message..." v-model="userInput"
         class="w-full resize-none break-words focus:outline-none" rows="1" ref="inputBox" @input="scaleChatBox()">
       </textarea>
-      
+
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
         class="lucide lucide-send-horizontal-icon lucide-send-horizontal"
