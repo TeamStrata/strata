fad9cfe7d6e4ba4335b380e68a4b38150e7b61e3
ajdj100 <ajairplane@gmail.com>
2025-08-12 00:31:59 -0400

    fix: added correct cursor state to new dashboard button

--- ui/src/components/Navbar.vue
+++ ui/src/components/Navbar.vue
@@ -206,7 +206,7 @@ loadDashboards();
             </SidebarGroup>
             <SidebarGroup>
                 <SidebarGroupLabel>Dashboards</SidebarGroupLabel>
-                <SidebarGroupAction @click="showCreateDashboardDialog" title="Add Dashboards">
+                <SidebarGroupAction @click="showCreateDashboardDialog" title="Add Dashboards" class="cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                             stroke-width="2" d="M5 12h14m-7-7v14" />
